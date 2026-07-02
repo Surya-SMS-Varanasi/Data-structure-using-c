0733644211d55a5a4ddbc4f2eb51ba493af9c761
Surya Pratap Singh <suryaprataps2024@gmail.com>
2026-07-02 23:16:43 +0530

    Add files via upload

---
+++ Day-2-array-basics/Prog2.c
@@ -0,0 +1,48 @@
+//1. Read and print an array.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int size, i;
+	printf("Enter the size of the array : ");
+	scanf("%d", &size);
+	
+	int *arr = (int *) malloc(size*sizeof(int));
+	
+	if(arr == NULL){
+		printf("Memory allocation failed!, pls enter a little size.");
+		return 0;
+	}	
+	else{
+		printf("Enter the array elements :\n");
+		for( i = 0; i < size; i++){
+			scanf("%d",&arr[i]);
+		}
+
+		// Printing the array elements
+		printf("\nThe array elements are,  arr = [ ");
+		for(i=0; i < size; i++){
+			if(i == size-1){
+				printf("%d ", arr[i]);
+				break;	
+			}
+			printf("%d, ", arr[i]);
+		}
+		printf("]\n\n");
+
+		printf("\nThe array elements in reversed order are,  arr = [ ");
+		for(i=size-1; i >= 0; i--){
+			if(i == size-1){
+				printf("%d ", arr[i]);
+				break;	
+			}
+			printf("%d, ", arr[i]);
+		}
+		printf("]\n\n");
+
+		
+		free(arr);
+	}
+	return 0;
+} 
