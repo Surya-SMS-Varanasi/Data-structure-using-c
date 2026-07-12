b9299e0c6a8efe8de8a90d8815c8193ca1837a1a
Surya Pratap Singh <suryaprataps2024@gmail.com>
2026-07-12 22:32:58 +0530

    Create prg10.c

---
+++ Day-3-element-searching/prg10.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+    int arr[]={23,45,65,88,44,33,768,6,23,12};
+    int size=10;
+    
+    for(int i = 0; i < size-1; i++)
+    {
+       int min = i;
+        for(int j = i+1; j< size; j++ ){
+            if(arr[min] > arr[j]){
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i]=temp;
+    }
+    for(int i = 0; i < size; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
