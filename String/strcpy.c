98ea4b8b4a934b088bca49d754f1937c4450d119
Surya Pratap Singh <suryaprataps2024@gmail.com>
2026-08-07 22:36:35 +0530

    Implement strcpy function in strcpy.c

---
+++ String/strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char source[] = "Hello World";
+    char destination[50];
+
+    int i = 0;
+
+    while (source[i] != '\0') {
+        destination[i] = source[i];
+        i++;
+    }
+
+    destination[i] = '\0';  // Add null character
+
+    printf("Source      : %s\n", source);
+    printf("Destination : %s\n", destination);
+
+    return 0;
+}
